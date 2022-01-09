ffffbcf0e756a49a3f1a45b7714da96fbaefe15a
Harsh <57228178+harsh3294@users.noreply.github.com>
2022-01-09 19:15:10 +0530

    added collection card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import CollectionCard from "./components/CollectionCard";
 import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="">
+    <div className="app">
       <Header />
+      <CollectionCard
+        id={0}
+        name={"Bandana Punk"}
+        traits={[{ value: 7 }]}
+        image="https://ipfs.thirdweb.com/ipfs/bafybeigqkficum3anns36jid3dxvc4yfauyuvqjulbg43n23qxn3ce3tyu"
+      />
     </div>
   );
 }
