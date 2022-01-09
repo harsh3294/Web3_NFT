40ff27e4c865585d3be8120800636ba92d739515
Harsh <57228178+harsh3294@users.noreply.github.com>
2022-01-09 19:28:21 +0530

    pulling data of nft from third web

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import "./App.css";
 import CollectionCard from "./components/CollectionCard";
 import Header from "./components/Header";
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 function App() {
+  const [punkListData, setPunkListData] = useState([]);
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get(
+        "https://testnets-api.opensea.io/assets?asset_contract_address=0x4E4873D9575247283dA13fefF7D1857Cb4effd51&order_direction=asc"
+      );
+      console.log(openseaData.data.assets);
+      setPunkListData(openseaData.data.assets);
+    };
+    return getMyNfts();
+  }, []);
   return (
     <div className="app">
       <Header />
