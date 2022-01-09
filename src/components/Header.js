944d5a19560bc2d19e0cc8819b64c730042d2d0d
Harsh <57228178+harsh3294@users.noreply.github.com>
2022-01-09 19:14:53 +0530

    completed with header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,39 @@
+import "./Header.css";
+import punkLogo from "../assets/header/cryptopunk-logo.png";
+import searchIcon from "../assets/header/search.png";
+import themeSwitch from "../assets/header/theme-switch.png";
 function Header() {
-  return <div></div>;
+  return (
+    <header className="header">
+      <div className="logoContainer">
+        <img src={punkLogo} className="punkLogo" alt="" />
+      </div>
+
+      <div className="searchBar">
+        <div className="searchIconContainer">
+          <img src={searchIcon} alt="" />
+        </div>
+        <input
+          type="text"
+          className="searchInput"
+          placeholder="Collection, Item or User..."
+        />
+      </div>
+
+      <div className="headerItems">
+        <p>Drops</p>
+        <p>MarketPlace</p>
+        <p>Create</p>
+      </div>
+
+      <div className="headerActions">
+        <div className="themeSwitchContainer">
+          <img src={themeSwitch} alt="" />
+        </div>
+      </div>
+      <div className="loginButton">GET IN</div>
+    </header>
+  );
 }
 
 export default Header;
